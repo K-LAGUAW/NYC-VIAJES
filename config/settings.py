3d414752453e053a3a69218444d7f57d7fe1c48c
K-LAGUAW <florestous@gmail.com>
2025-06-03 08:12:21 -0300

    latest database

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
-    {
+    {               
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'apps', 'web', 'templates'),
@@ -119,5 +119,3 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-SECURE_SSL_REDIRECT = True
